00c959b0fb2c42a0a7ba08ddbf9423f776199b24
Luke <lukeaveil@yahoo.com>
2016-06-03 16:05:40 +0100

    initial commit

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".fade").click(function(){
+		$(".contact").fadeIn("fast");
+	});
+});
+
+$(document).ready(function(){
+	$(".fade").click(function(){
+		$(".fade").fadeOut("fast");
+	});
+});
